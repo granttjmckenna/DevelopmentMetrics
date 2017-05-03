011915a0212fa204bc571333f32062ac1627bb0f
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-05-03 11:57:14 +0100

    Initial build failure rate calculations

--- DevelopmentMetrics.Tests/BuildMetricTests.cs
+++ DevelopmentMetrics.Tests/BuildMetricTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using DevelopmentMetrics.Models;
 using NUnit.Framework;
@@ -33,6 +34,85 @@ namespace DevelopmentMetrics.Tests
             Assert.That(firstBuildMetric.State.Equals("Finished", StringComparison.CurrentCultureIgnoreCase));
             Assert.That(firstBuildMetric.Status.Equals("Success", StringComparison.CurrentCultureIgnoreCase));
             Assert.That(!string.IsNullOrWhiteSpace(firstBuildMetric.AgentName));
+        }
+
+        [Test]
+        public void Should_calculate_failure_percentage()
+        {
+            var buildMetrics = GetBuildMetricsData();
+
+            var failingRate = new BuildCalculators().CalculateBuildFailingRate(buildMetrics);
+
+            Assert.That(failingRate, Is.EqualTo(30));
+        }
+
+        [Test]
+        public void Should_calculate_failure_percentage_for_project()
+        {
+            var buildMetrics = GetBuildMetricsData();
+
+            buildMetrics[9].ProjectId = "Exclude from calculation";
+
+            var failingRate = new BuildCalculators().CalculateProjectBuildFailingRateFor(buildMetrics, "Blah");
+
+            Assert.That(failingRate, Is.EqualTo(33.33));
+        }
+
+        [Test]
+        public void Should_calculate_failure_percentage_for_agent_name()
+        {
+            var buildMetrics = GetBuildMetricsData();
+
+            buildMetrics[9].AgentName = "Exclude from calculation";
+
+            var failingRate = new BuildCalculators().CalculateAgentBuildFailingRateFor(buildMetrics, "Blah");
+
+            Assert.That(failingRate, Is.EqualTo(33.33));
+        }
+
+        [Test]
+        public void Should_calculate_failure_percentage_by_project()
+        {
+            var buildMetrics = GetBuildMetricsData();
+
+            buildMetrics[9].ProjectId = "Different project id";
+
+            var projectBuildMetrics = new BuildCalculators().CalculateProjectBuildFailingRate(buildMetrics);
+
+            Assert.That(projectBuildMetrics.Keys.Count, Is.EqualTo(2));
+            Assert.That(projectBuildMetrics.First(c => c.Key.Equals("Blah")).Value, Is.EqualTo(33.33));
+            Assert.That(projectBuildMetrics.First(c => c.Key.Equals("Different project id")).Value, Is.EqualTo(0));
+        }
+
+        private List<BuildMetric> GetBuildMetricsData()
+        {
+            var dummyBuildMetrics = new List<BuildMetric>();
+
+            for (var i = 1; i < 11; i++)
+            {
+                dummyBuildMetrics.Add(
+                    new BuildMetric
+                    {
+                        ProjectId = "Blah",
+                        ProjectName = "Blah",
+                        BuildTypeId = "Blah_blah",
+                        BuildId = i,
+                        AgentName = "Blah",
+                        StartDateTime = DateTime.Now,
+                        FinishDateTime = DateTime.Now,
+                        QueueDateTime = DateTime.Now,
+                        State = "Finished",
+                        Status = GetStatus(i)
+                    }
+                );
+            }
+
+            return dummyBuildMetrics;
+        }
+
+        private string GetStatus(int i)
+        {
+            return ((i % 3) == 0) ? "Failure" : "Success";
         }
     }
 }
---
+++ DevelopmentMetrics/Models/BuildCalculators.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevelopmentMetrics.Models
+{
+    public class BuildCalculators
+    {
+        public double CalculateBuildFailingRate(List<BuildMetric> buildMetrics)
+        {
+            return CalculateFailingRate(buildMetrics);
+        }
+
+        public Dictionary<string, double> CalculateProjectBuildFailingRate(List<BuildMetric> buildMetrics)
+        {
+            var projectBuildMetrics = new Dictionary<string, double>();
+
+            foreach (var buildMetric in buildMetrics)
+            {
+                if (projectBuildMetrics.ContainsKey(buildMetric.ProjectId))
+                    continue;
+
+                var failureRate = new BuildCalculators().CalculateProjectBuildFailingRateFor(buildMetrics,
+                    buildMetric.ProjectId);
+
+                projectBuildMetrics.Add(buildMetric.ProjectId, failureRate);
+            }
+
+            return projectBuildMetrics;
+        }
+
+        public double CalculateProjectBuildFailingRateFor(List<BuildMetric> buildMetrics, string projectId)
+        {
+            var projectBuildMetrics = buildMetrics
+                .Where(b => b.ProjectId.Equals(projectId, StringComparison.InvariantCultureIgnoreCase))
+                .ToList();
+
+            return CalculateFailingRate(projectBuildMetrics);
+        }
+
+        public double CalculateAgentBuildFailingRateFor(List<BuildMetric> buildMetrics, string agentName)
+        {
+            var projectBuildMetrics = buildMetrics
+                .Where(b => b.AgentName.Equals(agentName, StringComparison.InvariantCultureIgnoreCase))
+                .ToList();
+
+            return CalculateFailingRate(projectBuildMetrics);
+        }
+
+        private static double CalculateFailingRate(IReadOnlyCollection<BuildMetric> buildMetrics)
+        {
+            var total = buildMetrics.Count;
+
+            var failing =
+                buildMetrics.Count(
+                    b =>
+                        b.State.Equals("Finished", StringComparison.InvariantCultureIgnoreCase) &&
+                        b.Status.Equals("Failure", StringComparison.CurrentCultureIgnoreCase));
+
+            var failingRate = Math.Round((double) (100 * failing) / total, 2);
+
+            return failingRate;
+        }
+    }
+}
