5918e2313c6ddd5fefd992d0ce557809baa169ee
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-11-21 08:09:49 +0000

    Refactor classes into appropriate location

---
+++ DevelopmentMetrics/Models/CardCount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevelopmentMetrics.Models
+{
+    public class CardCount
+    {
+        private readonly IEnumerable<Card> _cards;
+        public DateTime Date { get; set; }
+        public int DoneTotal { get; set; }
+        public int Total { get; set; }
+
+        private CardCount() { }
+
+        public CardCount(IEnumerable<Card> cards)
+        {
+            _cards = cards;
+        }
+
+        public List<CardCount> GetCardCountByDayFrom(DateTime dateTime)
+        {
+            var maxCreatedDate = _cards.Max(c => c.CreatedDate);
+
+            var days = Enumerable.Range(0, 1 + maxCreatedDate.Subtract(dateTime).Days)
+                .Select(o => dateTime.AddDays(o)).ToList();
+
+            return (from day in days
+                let countByDay = GetCardCountsFor(AllPredicateFor(day))
+                let doneCountByDay = GetCardCountsFor(DonePredicateFor(day))
+                select new CardCount
+                {
+                    Date = day,
+                    DoneTotal = doneCountByDay,
+                    Total = countByDay
+                }).ToList();
+        }
+
+        private static Func<Card, bool> AllPredicateFor(DateTime day)
+        {
+            return c => c.CreatedDate <= day;
+        }
+
+        private static Func<Card, bool> DonePredicateFor(DateTime dateTime)
+        {
+            return c => c.CreatedDate <= dateTime && c.Status.Equals(CardStatus.Status.Done);
+        }
+
+        private int GetCardCountsFor(Func<Card, bool> func)
+        {
+            return _cards.Count(func);
+        }
+    }
+}
---
+++ DevelopmentMetrics/Models/CardMetric.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevelopmentMetrics.Models
+{
+    public class CardMetric
+    {
+        private readonly IEnumerable<Card> _cards;
+
+        public CardMetric(IEnumerable<Card> cards)
+        {
+            _cards = cards;
+        }
+
+        public Dictionary<CardStatus.Status, int> GetCountByStatus()
+        {
+            var result = new Dictionary<CardStatus.Status, int>
+            {
+                {CardStatus.Status.Todo, _cards.Count(c => c.Status.Equals(CardStatus.Status.Todo))},
+                {CardStatus.Status.Doing, _cards.Count(c => c.Status.Equals(CardStatus.Status.Doing))},
+                {CardStatus.Status.Done, _cards.Count(c => c.Status.Equals(CardStatus.Status.Done))}
+            };
+
+            return result;
+        }
+
+        public int CalculateLeadTimeFor(DateTime calculationDate)
+        {
+            var cardPosition = _cards
+                .OrderBy(c => c.CreatedDate)
+                .Count(DonePredicateFor(calculationDate));
+
+            var cardDate = _cards.OrderBy(c => c.CreatedDate).Take(cardPosition).Max(c => c.CreatedDate);
+
+            return (calculationDate - cardDate).Days;
+        }
+
+        private static Func<Card, bool> DonePredicateFor(DateTime calculationDate)
+        {
+            return c => c.CreatedDate <= calculationDate && c.Status.Equals(CardStatus.Status.Done);
+        }
+
+        public int CalculateWorkInProcessFor(DateTime calculationDateTime)
+        {
+            var cardCount = _cards.Count(c => c.CreatedDate <= calculationDateTime);
+
+            var doneCardCount = _cards.Count(DonePredicateFor(calculationDateTime));
+
+            return cardCount - doneCardCount;
+        }
+    }
+}
