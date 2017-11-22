e5d2eff7fd7b9f56f827f71717fd576b4ef44ebf
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-11-22 08:27:06 +0000

    Add card count by day to view

--- DevelopmentMetrics.Website/Models/CardsViewModel.cs
+++ DevelopmentMetrics.Website/Models/CardsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using DevelopmentMetrics.Cards;
 
 namespace DevelopmentMetrics.Website.Models
@@ -10,6 +11,11 @@ namespace DevelopmentMetrics.Website.Models
         public CardsViewModel(IEnumerable<Card> cards)
         {
             _cards = cards;
+        }
+
+        public List<CardCount> GetCardCountByDay()
+        {
+            return new CardCount(_cards).GetCardCountByDayFrom(DateTime.Now.AddDays(-42));
         }
 
         public Dictionary<CardStatus.Status, int> GetCardCountByStatus()
