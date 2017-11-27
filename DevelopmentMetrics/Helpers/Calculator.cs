1983d4069ad79fd89ce83ebd06d1d668d70e9513
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-11-27 20:59:13 +0000

    Refactor: rename calculator parameter

--- DevelopmentMetrics/Helpers/Calculator.cs
+++ DevelopmentMetrics/Helpers/Calculator.cs
@@ -4,11 +4,11 @@ namespace DevelopmentMetrics.Helpers
 {
     public static class Calculator
     {
-        public static double Percentage(int nominator, int denominator)
+        public static double Percentage(int numerator, int denominator)
         {
             return (denominator == 0)
                 ? 0
-                : Math.Round((double)nominator / denominator, 2);
+                : Math.Round((double)numerator / denominator, 2);
         }
     }
 }
