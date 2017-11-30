07d9d2f132cb6b03521b1c06501c892f2ac4ad3e
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-11-30 08:25:29 +0000

    Add WebClient interface to execute GET request

--- DevelopmentMetrics/Repository/LeanKitWebClient.cs
+++ DevelopmentMetrics/Repository/LeanKitWebClient.cs
@@ -12,21 +12,32 @@ namespace DevelopmentMetrics.Repository
 
     public class LeanKitWebClient : ILeanKitWebClient
     {
+        private readonly IWebClient _webClient;
         private const string ApiDomain = "https://ehl.leankit.com/kanban/api";
         private const int BoardId = 311598445;
+
+        public LeanKitWebClient(IWebClient webClient)
+        {
+            _webClient = webClient;
+        }
 
         public string GetBoardData()
         {
             var url = $"{ApiDomain}/boards/{BoardId}";
 
-            return Get(url);
+            return ExecuteGetRequest(url);
         }
 
         public string GetCardDataFor(int cardId)
         {
             var url = $"{ApiDomain}/board/{BoardId}/GetCard/{cardId}";
 
-            return Get(url);
+            return ExecuteGetRequest(url);
+        }
+
+        private string ExecuteGetRequest(string url)
+        {
+            return _webClient.Get(url);
         }
 
         private string Get(string url)
