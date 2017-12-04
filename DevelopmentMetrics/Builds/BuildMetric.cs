f0bc377d8ae95e07c009949b3c5fac1a83f37f71
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-12-04 08:48:30 +0000

    *****WIP******

---
+++ DevelopmentMetrics/Builds/BuildMetric.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevelopmentMetrics.Helpers;
+
+namespace DevelopmentMetrics.Builds
+{
+    public class BuildMetric
+    {
+        private BuildMetric() { }
+
+        private readonly List<Build> _builds;
+
+        public string BuildMonth { get; set; }
+
+        public double FailureRate { get; set; }
+
+        public BuildMetric(List<Build> builds)
+        {
+            _builds = builds;
+        }
+        public List<BuildMetric> CalculateBuildFailingRateByMonthFrom(DateTime fromDate)
+        {
+            var results = new List<BuildMetric>();
+
+            for (var i = 0; i < 12; i++)
+            {
+                var queryDate = fromDate.AddMonths(i);
+
+                var monthBuildMetrics =
+                    _builds
+                        .Where(b => b.State.Equals("Finished", StringComparison.InvariantCultureIgnoreCase)
+                                    && b.StartDateTime.Month.Equals(queryDate.Month)
+                                    && b.StartDateTime.Year.Equals(queryDate.Year))
+                        .ToList();
+
+                if (!monthBuildMetrics.Any())
+                    continue;
+
+                var total = monthBuildMetrics.Count();
+
+                var failing = monthBuildMetrics
+                    .Count(b => b.Status.Equals(Helpers.BuildStatus.Failure.ToString(), StringComparison.CurrentCultureIgnoreCase));
+
+                var failingRate = Calculator.Percentage(failing, total);
+
+                results.Add(new BuildMetric
+                {
+                    BuildMonth = queryDate.ToString("MMM-yyyy"),
+                    FailureRate = failingRate
+                });
+            }
+
+            return results;
+        }
+    }
+}
