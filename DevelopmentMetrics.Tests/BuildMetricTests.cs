b91103958fe562bc599beab502059827e6df2848
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-12-05 08:43:35 +0000

    Remove FakeRepository class

--- DevelopmentMetrics.Tests/BuildMetricTests.cs
+++ DevelopmentMetrics.Tests/BuildMetricTests.cs
@@ -9,33 +9,6 @@ namespace DevelopmentMetrics.Tests
     [TestFixture]
     public class BuildMetricTests
     {
-        private FakeRepository _fakeRepository;
-
-        [SetUp]
-        public void SetUp()
-        {
-            _fakeRepository = new FakeRepository();
-        }
-
-        [Test]
-        public void Should_populate_build_metric()
-        {
-            var buildMetrics = new Project(_fakeRepository).GetBuildMetrics();
-
-            var firstBuildMetric = buildMetrics.First(b => b.ProjectName.Equals("Admin", StringComparison.InvariantCultureIgnoreCase));
-
-            Assert.That(firstBuildMetric.ProjectId, Is.EqualTo("Admin"));
-            Assert.That(firstBuildMetric.ProjectName, Is.EqualTo("Admin"));
-            Assert.That(firstBuildMetric.BuildTypeId, Is.EqualTo("Admin_00UpdateCoreNuGetPackages"));
-            Assert.That(firstBuildMetric.BuildId, Is.GreaterThan(0));
-            Assert.That(firstBuildMetric.StartDateTime, Does.Not.EqualTo(new DateTime(0001, 01, 01, 00, 00, 00)));
-            Assert.That(firstBuildMetric.FinishDateTime, Does.Not.EqualTo(new DateTime(0001, 01, 01, 00, 00, 00)));
-            Assert.That(firstBuildMetric.QueueDateTime, Does.Not.EqualTo(new DateTime(0001, 01, 01, 00, 00, 00)));
-            Assert.That(firstBuildMetric.State.Equals("Finished", StringComparison.CurrentCultureIgnoreCase));
-            Assert.That(firstBuildMetric.Status.Equals("Success", StringComparison.CurrentCultureIgnoreCase));
-            Assert.That(!string.IsNullOrWhiteSpace(firstBuildMetric.AgentName));
-        }
-
         [Test]
         public void Should_calculate_failure_percentage()
         {
