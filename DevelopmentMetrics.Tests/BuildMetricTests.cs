357ac3b1a7f7826bbc84e3dde3605ca3f31d7741
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-12-11 08:45:19 +0000

    Add CalculatorTests class for calculator methods

--- DevelopmentMetrics.Tests/BuildMetricTests.cs
+++ DevelopmentMetrics.Tests/BuildMetricTests.cs
@@ -137,40 +137,6 @@ namespace DevelopmentMetrics.Tests
             Assert.That(metrics.First().RecoveryTime, Is.EqualTo(1));
         }
 
-
-        [Test]
-        public void Return_for_standard_deviation_when_list_is_empty()
-        {
-            var values = new List<double>();
-
-            var standardDeviation = Calculator.CalculateStandardDeviation(values);
-
-            Assert.That(standardDeviation, Is.EqualTo(0));
-        }
-
-        [Test]
-        public void Return_standard_deviation_when_list_is_not_empty()
-        {
-            var values = new List<double> { 1d, 2d, 3d, 2d, 1d };
-
-            var standardDeviation = Calculator.CalculateStandardDeviation(values);
-
-            Assert.That(standardDeviation, Is.GreaterThan(0.83d));
-            Assert.That(standardDeviation, Is.LessThan(0.84d));
-        }
-
-        [Test]
-        public void Return_standard_deviation_of_recovery_time_in_hours()
-        {
-            var doubles = new List<double> { 2100000d, 2400000d, 2760000d };
-
-            var standardDeviationInHours =
-                Calculator.ConvertMillisecondsToHours(
-                    Calculator.CalculateStandardDeviation(doubles));
-
-            Assert.That(standardDeviationInHours, Is.EqualTo(0));
-        }
-
         [Test]
         public void Return_collection_of_distinct_build_type_ids()
         {
---
+++ DevelopmentMetrics.Tests/CalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using DevelopmentMetrics.Helpers;
+using NUnit.Framework;
+
+namespace DevelopmentMetrics.Tests
+{
+    public class CalculatorTests
+    {
+        [Test]
+        public void Return_for_standard_deviation_when_list_is_empty()
+        {
+            var values = new List<double>();
+
+            var standardDeviation = Calculator.CalculateStandardDeviation(values);
+
+            Assert.That(standardDeviation, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Return_standard_deviation_when_list_is_not_empty()
+        {
+            var values = new List<double> { 1d, 2d, 3d, 2d, 1d };
+
+            var standardDeviation = Calculator.CalculateStandardDeviation(values);
+
+            Assert.That(standardDeviation, Is.GreaterThan(0.83d));
+            Assert.That(standardDeviation, Is.LessThan(0.84d));
+        }
+
+        [Test]
+        public void Return_standard_deviation_of_recovery_time_in_hours()
+        {
+            var doubles = new List<double> { 2100000d, 2400000d, 2760000d };
+
+            var standardDeviationInHours =
+                Calculator.ConvertMillisecondsToHours(
+                    Calculator.CalculateStandardDeviation(doubles));
+
+            Assert.That(standardDeviationInHours, Is.EqualTo(0));
+        }
+
+    }
+}
