274dbe1d143d9bc0b58f8371cf0251c9d1f60613
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-12-12 19:17:35 +0000

    Add partial view for build list

---
+++ DevelopmentMetrics.Website/Models/PartialViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using DevelopmentMetrics.Builds;
+
+namespace DevelopmentMetrics.Website.Models
+{
+    public class PartialViewModel
+    {
+        public List<FailureRate> FailureRates { get; }
+        public string CssStyle { get; }
+        public string DivId { get; }
+
+        public PartialViewModel(List<FailureRate> failureRates, string cssStyle, string divId)
+        {
+            FailureRates = failureRates;
+            CssStyle = cssStyle;
+            DivId = divId;
+        }
+    }
+}
