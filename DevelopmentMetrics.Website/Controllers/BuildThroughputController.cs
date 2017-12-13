af9e8857283533e201a62fad99ca68bc13a27541
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-12-13 16:25:51 +0000

    Add BuildThroughput controller

---
+++ DevelopmentMetrics.Website/Controllers/BuildThroughputController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace DevelopmentMetrics.Website.Controllers
+{
+    public class BuildThroughputController : Controller
+    {
+        // GET: BuildThroughput
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
