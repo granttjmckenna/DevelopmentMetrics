ded6079c4203fa5d728cc434a4dbe4b6b2940daa
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-12-13 16:29:58 +0000

    Add method for returning JSON Chart data

--- DevelopmentMetrics.Website/Controllers/BuildThroughputController.cs
+++ DevelopmentMetrics.Website/Controllers/BuildThroughputController.cs
@@ -1,13 +1,31 @@
 ﻿using System.Web.Mvc;
+using DevelopmentMetrics.Builds;
+using DevelopmentMetrics.Helpers;
 
 namespace DevelopmentMetrics.Website.Controllers
 {
     public class BuildThroughputController : Controller
     {
+        private readonly IBuild _build;
+        private readonly ITellTheTime _tellTheTime;
+
+        public BuildThroughputController(IBuild build, ITellTheTime tellTheTime)
+        {
+            _build = build;
+            _tellTheTime = tellTheTime;
+        }
         // GET: BuildThroughput
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetBuildThroughputChartDataFor(int numberOfWeeks)
+        {
+            var buildData = new BuildThroughput(_build, _tellTheTime).CalculateBuildIntervalByWeekFor(numberOfWeeks);
+
+            return Json(buildData);
         }
     }
 }
