3ef50865e8af414ce7c105a2dbafbf4afdd3d118
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-12-13 08:34:43 +0000

    Replace chart data filter with partial view and ChartFilterViewModel class

--- DevelopmentMetrics.Website/Models/BuildStabilityViewModel.cs
+++ DevelopmentMetrics.Website/Models/BuildStabilityViewModel.cs
@@ -46,5 +46,23 @@ namespace DevelopmentMetrics.Website.Models
 
             return results;
         }
+
+        public Dictionary<string, string> GetBuildWeeksList()
+        {
+            var results = new Dictionary<string, string>
+            {
+                {"6 weeks (default)", "6"},
+                {"7 weeks", "7"},
+                {"8 weeks", "8"},
+                {"9 weeks", "9"},
+                {"10 weeks", "10"},
+                {"15 weeks", "15"},
+                {"20 weeks", "20"},
+                {"All", "-2"}
+            };
+
+
+            return results;
+        }
     }
 }
