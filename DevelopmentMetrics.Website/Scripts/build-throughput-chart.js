3e7027442e05472b581653cef4dee6453bf848e4
Grant McKenna <grant.mckenna@firsthelpline.com>
2017-12-13 16:39:25 +0000

    Add javascript methods for rendering build throughput chart

--- DevelopmentMetrics.Website/Scripts/build-throughput-chart.js
+++ DevelopmentMetrics.Website/Scripts/build-throughput-chart.js
@@ -1,3 +1,79 @@
 ﻿function filterBuildThroughputChart() {
-    //does nothing!
+    drawBuildThroughputChart(6);
+};
+
+function drawBuildThroughputChart(weeks) {
+
+    $.ajax({
+        url: "/BuildThroughput/GetBuildThroughputChartDataFor",
+        dataType: "json",
+        data: {
+            numberOfWeeks: weeks
+        },
+        type: "POST",
+        error: function () {
+            showChartMessage("Error retrieving chart data");
+        },
+        beforeSend: function () {
+            showChartLoading();
+        },
+        success: function (data) {
+            renderBuildThroughputChartData(data);
+        },
+        complete: function () {
+            showChartLoaded();
+        }
+    });
+
+    return false;
+};
+
+function renderBuildThroughputChartData(data) {
+    var dataTable = new google.visualization.DataTable();
+    dataTable.addColumn("date", "Day");
+    dataTable.addColumn("number", "Build Interval Time");
+    dataTable.addColumn("number", "Build Interval Time (std dev)");
+
+    $.each(data,
+        function (i, item) {
+            dataTable.addRows([[new Date(getDateIfDate(item.Date)), item.BuildIntervalTime, item.BuildIntervalTimeStdDev]]);
+        });
+
+    var options = {
+        title: getBuildChartTitle(),
+        titleTextStyle: {
+            fontSize: 20,
+            italic: false
+        },
+        curveType: "function",
+        legend: {
+            position: "bottom"
+        },
+        hAxis: {
+            gridlines: {
+                color: "transparent"
+            },
+            title: "Date",
+            format: "dd MMM",
+            titleTextStyle: {
+                fontSize: 20,
+                italic: false
+            }
+        },
+        vAxis: {
+            gridlines: {
+                color: "transparent"
+            },
+            title: "Hours",
+            titleTextStyle: {
+                fontSize: 20,
+                italic: false
+            }
+        },
+        colors: ["#FF0000", "#34A853", "#FF6600"]
+    };
+
+    drawChart(dataTable, options);
+
+    return false;
 };
