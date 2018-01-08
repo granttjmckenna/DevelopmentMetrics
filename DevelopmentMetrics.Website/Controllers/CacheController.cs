6aee766be0e153dfaac71bf2fb62238c550c289f
Grant McKenna <grant.mckenna@firsthelpline.com>
2018-01-08 08:33:05 +0000

    ****Wip****

--- DevelopmentMetrics.Website/Controllers/CacheController.cs
+++ DevelopmentMetrics.Website/Controllers/CacheController.cs
@@ -1,15 +1,30 @@
 ﻿using System.Web.Mvc;
+using DevelopmentMetrics.Builds;
+using DevelopmentMetrics.Cards;
+using DevelopmentMetrics.Helpers;
+using DevelopmentMetrics.Website.Models;
 
 namespace DevelopmentMetrics.Website.Controllers
 {
     public class CacheController : Controller
     {
-        [HttpGet]
-        public JsonResult GetBuildCacheStatus()
+        private ICard _card;
+        private IBuild _build;
+        private ICacheChecker _cacheChecker;
+
+        public CacheController(ICacheChecker cacheChecker,IBuild build, ICard card)
         {
+            _cacheChecker = cacheChecker;
+            _build = build;
+            _card = card;
+        }
 
+        [HttpGet]
+        public JsonResult ReturnTrueWhenBuildDataCached()
+        {
+            new Cache(_cacheChecker, _build, _card).IsBuildDataCached();
 
-            return Json("{'IsCached':'true'}");
+            return Json(true);
         }
     }
 }
