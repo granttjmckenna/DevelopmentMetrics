3ae42d8ff6696b40456833c6194b3486dec3c1a2
Grant McKenna <grant.mckenna@firsthelpline.com>
2018-01-10 15:05:19 +0000

    Add partial view for build options

---
+++ DevelopmentMetrics.Website/Models/ViewButton.cs
@@ -0,0 +1,14 @@
+﻿namespace DevelopmentMetrics.Website.Models
+{
+    public class ViewButton
+    {
+        public string Label { get; }
+        public string Action { get; }
+
+        public ViewButton(string label, string action)
+        {
+            Label = label;
+            Action = action;
+        }
+    }
+}
