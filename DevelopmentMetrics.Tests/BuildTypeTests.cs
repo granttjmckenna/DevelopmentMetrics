73243a08bcf7a64905675f57fa49f66a73019305
Grant McKenna <grant.mckenna@firsthelpline.com>
2018-01-25 19:48:21 +0000

    Add initial test for distinct build types

---
+++ DevelopmentMetrics.Tests/BuildTypeTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using DevelopmentMetrics.Builds;
+using NUnit.Framework;
+
+namespace DevelopmentMetrics.Tests
+{
+    [TestFixture]
+    public class BuildTypeTests
+    {
+        [Test]
+        public void Return_distinct_list_of_build_types()
+        {
+            var builds = GetBuilds();
+
+            var list = new BuildType().GetDistinctBuildTypeIds(builds);
+
+            Assert.That(list.Count, Is.EqualTo(1));
+        }
+
+        private List<Build> GetBuilds()
+        {
+            return new List<Build>
+            {
+                new Build
+                {
+                    BuildTypeId = "EHLInsight_10Test"
+                }
+            };
+        }
+    }
+}
