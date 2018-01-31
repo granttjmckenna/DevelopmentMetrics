21e45c54b2d76916f937845f307d1aa2e81590ee
Grant McKenna <grant.mckenna@firsthelpline.com>
2018-01-31 09:41:51 +0000

    Inline method

--- DevelopmentMetrics/Builds/Metrics/BuildStabilityMetric.cs
+++ DevelopmentMetrics/Builds/Metrics/BuildStabilityMetric.cs
@@ -44,23 +44,11 @@ namespace DevelopmentMetrics.Builds.Metrics
                 RecoveryTime = CalculateAverageRecoveryTimeInHoursFor(Intervals),
                 RecoveryTimeStdDev = Calculator.ConvertMillisecondsToHours(
                     Calculator.CalculateStandardDeviation(Intervals)),
-                IgnoredTestCount = SumIgnoredTestCountsFor(Builds)
+                IgnoredTestCount = Builds.Sum(build => build.IgnoredTestCount)
             });
 
             Intervals.Clear();
             Builds.Clear();
-        }
-
-        private int SumIgnoredTestCountsFor(List<Build> builds)
-        {
-            int total = 0;
-
-            foreach (var build in Builds)
-            {
-                total += build.IgnoredTestCount;
-            }
-
-            return total;
         }
 
         public List<BuildStabilityMetric> GetResults()
